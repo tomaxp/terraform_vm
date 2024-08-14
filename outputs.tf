52dfc770456f4e7bc932afd24f7b21a96cb61c95
extu99tpaw <tomasz.pawlak@Omnilogy.pl>
2024-08-14 17:58:29 +0200

    mac address output

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,7 @@
 output "vm_id" {
   value = var.add_additional_disk ? xenserver_vm.new_vm_with_disk[0].id : xenserver_vm.new_vm_without_disk[0].id
 }
+
+output "vm_mac_address" {
+  value = var.add_additional_disk ? tolist(xenserver_vm.new_vm_with_disk[0].network_interface)[0].mac : tolist(xenserver_vm.new_vm_without_disk[0].network_interface)[0].mac
+}
