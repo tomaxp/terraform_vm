5048998a3683b264911bf378e403f438c054c420
extu99tpaw <tomasz.pawlak@Omnilogy.pl>
2024-09-11 15:14:27 +0200

    test

--- outputs.tf
+++ outputs.tf
@@ -5,3 +5,28 @@ output "vm_id" {
 output "vm_mac_address" {
   value = var.add_additional_disk ? tolist(xenserver_vm.new_vm_with_disk[0].network_interface)[0].mac : tolist(xenserver_vm.new_vm_without_disk[0].network_interface)[0].mac
 }
+
+output "vm_mac_address" {
+  value = xenserver_vm.new_vm_without_disk[0].network_interface[0].mac
+}
+
+output "new_vm_name" {
+  value = var.new_vm_name
+}
+
+output "template_name" {
+  value = var.template_name
+}
+
+output "memory" {
+  value = var.memory
+}
+
+output "cpus" {
+  value = var.cpus
+}
+
+output "disk_size" {
+  value = var.additional_disk_size
+}
+
